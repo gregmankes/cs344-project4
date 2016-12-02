f9b4c528d573fb444fbacb8a1ded2bf77260f597
gregmankes <gregmankes@gmail.com>
2016-12-02 15:08:06 -0500

    commented otp_enc and dec

--- otp_enc.c
+++ otp_enc.c
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * otp_enc.c 
+ *
+ * Author: Gregory Mankes
+ * Takes a file, sends it to a given daemon with a key and receives the
+ * encoded file back
+ ******************************************************************************/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,8 +12,6 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#include <arpa/inet.h>
-#include <dirent.h>
 #include <unistd.h>
 #include <fcntl.h>
 
@@ -68,8 +73,13 @@ void connect_socket(int sockfd, struct addrinfo * res){
 	}
 }
 
-
-void sendFile(int fd, int sockfd) {
+/*******************************************************************************
+ * void send_file(int, int)
+ *
+ * Sends a file over the socket
+ * Args: a file descriptor and a socket file descriptor
+ ******************************************************************************/
+void send_file(int fd, int sockfd) {
 	// keep track of how many bytes read and wrote
 	int nread;
 	int nwrite;
@@ -99,6 +109,12 @@ void sendFile(int fd, int sockfd) {
 	nread = read(sockfd, buffer, sizeof(buffer));
 }
 
+/*******************************************************************************
+ * int handshake(int)
+ *
+ * Completes a handshake with a daemon of the same type
+ * Args: a socket file descriptor
+ ******************************************************************************/
 int handshake(int sockfd){
 	//	printf("Verifying identity with daemon\n");
 	char * identity = "opt_enc";
@@ -106,20 +122,27 @@ int handshake(int sockfd){
 	// get the response from the server
 	char buffer[100];
 	memset(buffer, 0, sizeof(buffer));
-	//printf("Receiving response from daemon\n");
 	recv(sockfd, buffer, sizeof(buffer),0);
 	int to_return = 0;
+	// set the value of to_return
 	if(strcmp(buffer, "Valid") == 0){
 		to_return = 1;
 	}
 	return to_return;
 }
 
+/*******************************************************************************
+ * char * recv_file(int, int)
+ *
+ * Receives a file of a specified size and returns its contents in a string
+ * Args: a socket file descriptor and a message length
+ ******************************************************************************/
 char * recv_file(int new_fd, int message_length){
+	// allocate a receive buffer and read variables
 	char * to_receive = malloc(message_length * sizeof(char));
 	int nread = 0;
 	int i = 0;
-	//printf("Receiving the file\n");
+	// begin receiving the file
 	for(; i< message_length; i+= nread){
 		nread = read(new_fd, to_receive + i, message_length -i);
 		if(nread < 0){
@@ -133,10 +156,17 @@ char * recv_file(int new_fd, int message_length){
 	return to_receive;
 }
 
-
+/*******************************************************************************
+ * int check_file_and_get_length(int)
+ *
+ * Gets the file's length and makes sure it contains valid characters
+ * Args: a file descriptor
+ ******************************************************************************/
 int check_file_and_get_length(int fd){
+	// allocate buffer
 	char buffer[100];
 	memset(buffer, 0, sizeof(buffer));
+	// go through file 1 char at a time and check for invalid chars
 	while(read(fd, buffer, 1) != 0){
 		if(((buffer[0] < 'A' || buffer[0] > 'Z') &&
 			buffer[0] != ' ') && buffer[0] != '\n'){
@@ -144,9 +174,17 @@ int check_file_and_get_length(int fd){
 			exit(1);
 		}
 	}
+	// return its length
 	return lseek(fd, 0, SEEK_END);
 }
 
+
+/*******************************************************************************
+ * void handle_request(int, char *, char *)
+ *
+ * handles the request to the daemon
+ * Args: a socket file descriptor, a file name and a key name
+ ******************************************************************************/
 void handle_request(int sockfd, char * filename, char * keyname){
 	// begin by verifying identity
 	int is_valid = handshake(sockfd);
@@ -172,8 +210,6 @@ void handle_request(int sockfd, char * filename, char * keyname){
 		fprintf(stderr, "Error: Key is too short\n");
 		exit(1);
 	}
-	close(key_fd);
-	close(file_fd);
 	// tell the daemon the size of the strings
 	char file_length_s[20];
 	memset(file_length_s, 0, sizeof(file_length_s));
@@ -187,13 +223,15 @@ void handle_request(int sockfd, char * filename, char * keyname){
 	// sending the length of the key and echoing back
 	send(sockfd, key_length_s, strlen(key_length_s), 0);
 	recv(sockfd, key_length_s, sizeof(key_length_s), 0);
-	int filefd = open(filename, O_RDONLY);
-	sendFile(filefd, sockfd);
-	int keyfd = open(keyname,O_RDONLY);
-	sendFile(keyfd, sockfd);
-	close(filefd);
-	close(keyfd);
+	// send them
+	send_file(file_fd, sockfd);
+	send_file(key_fd, sockfd);
+	// close the files
+	close(file_fd);
+	close(key_fd);
+	// get the encrypted file back
 	char * encrypted = recv_file(sockfd, file_length);
+	// print it and free the space
 	printf("%s", encrypted);
 	free(encrypted);
 }
@@ -206,11 +244,13 @@ void handle_request(int sockfd, char * filename, char * keyname){
  * Args: the command lin args
  ******************************************************************************/
 int main(int argc, char *argv[]){
+	// check the number of args
 	if(argc < 4){
 		fprintf(stderr, "Invalid number of arguments\n");
 		fprintf(stderr, "Usage: opt_enc filename keyname portnumber\n");
 		exit(1);
 	}
+	// check for invalid chars
 	int fd = open(argv[1], O_RDONLY);
 	if(fd < 0){
 		fprintf(stderr, "There was an error opening %s\n", argv[1]);
@@ -218,6 +258,7 @@ int main(int argc, char *argv[]){
 	}
 	check_file_and_get_length(fd);
 	close(fd);
+	// check for invalid chars
 	fd = open(argv[2], O_RDONLY);
 	if(fd < 0){
 		fprintf(stderr, "There was an error opening %s\n", argv[2]);
@@ -225,9 +266,11 @@ int main(int argc, char *argv[]){
 	}
 	check_file_and_get_length(fd);
 	close(fd);
+	// set up socket
 	struct addrinfo * res = create_address_info(argv[3]);
 	int sockfd = create_socket(res);
 	connect_socket(sockfd, res);
+	// handle request
 	handle_request(sockfd, argv[1], argv[2]);
 	freeaddrinfo(res);
 	close(sockfd);
